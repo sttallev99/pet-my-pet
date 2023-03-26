528fb48e25c4eec3f79a8e213fa8f3efe59f011c
Георги Сталев <sttallev99@gmail.com>
2023-03-26 19:51:19 +0300

    separate components and make dynamic navigation

---
+++ src/components/Header/Header.js
@@ -0,0 +1,36 @@
+import { Link } from 'react-router-dom';
+
+export default function Header({
+    isAuthenticated,
+    user
+}) {
+    let guestNavigation = (
+        <div id="guest">
+            <Link className="button" to="/login">Login</Link>
+            <Link className="button" to="/register">Register</Link>
+        </div>
+    );
+
+    let userNavigation = (
+        <div id="user">
+            <span>Welcome, {user}</span>
+            <Link className="button" to="/my-pet">My Pets</Link>
+            <Link className="button" to="/create">Add Pet</Link>
+            <Link className="button" to="#">Logout</Link>
+        </div>
+    );
+
+    return (
+        <header id="site-header">
+            <nav className="navbar">
+                <section className="navbar-dashboard">
+                    <Link to="/">Dashboard</Link>
+                    { isAuthenticated
+                        ? userNavigation
+                        : guestNavigation
+                    }
+                </section>
+            </nav>
+        </header>
+    )
+}
